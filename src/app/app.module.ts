03af5421421b23b0270f7e6016046f7232d37923
saitejavaddepally <saitejavaddepally11@gmail.com>
2022-08-11 09:38:19 +0000

    added receiver page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ReceiverComponent } from './receiver/receiver.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReceiverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AutocompleteLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
